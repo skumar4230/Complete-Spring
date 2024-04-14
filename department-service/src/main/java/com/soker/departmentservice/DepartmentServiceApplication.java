97c501b67d7e2dc9e91dda0995a31059a07c7569
sourav-code <skumar4230@gmail.com>
2024-04-14 11:39:00 +0530

    issue resolved with adding spring cloud config dependency in pom

--- department-service/src/main/java/com/soker/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/soker/departmentservice/DepartmentServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
+
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
