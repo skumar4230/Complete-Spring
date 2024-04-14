3cac02ffc856358b2b07b9e2945103a1a86d158f
sourav-code <skumar4230@gmail.com>
2024-04-14 11:10:11 +0530

    some issue till this point with ports

---
+++ department-service/src/main/java/com/soker/departmentservice/model/Department.java
@@ -0,0 +1,20 @@
+package com.soker.departmentservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class Department {
+    private Long id;
+    private String name;
+    private List<Employee> employees = new ArrayList<Employee>();
+
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ department-service/src/main/java/com/soker/departmentservice/model/Employee.java
@@ -0,0 +1,5 @@
+package com.soker.departmentservice.model;
+
+public record Employee(Long id, Long departmentId,String name, int age, String position) {
+
+}
